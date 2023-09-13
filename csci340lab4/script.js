fb8d5e186360cf9629be311adf455f736efbf6be
Ketsia <dusengeketty1@gmail.com>
2023-09-13 12:29:26 -0500

    added a second api and connected it to the first using one button

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
     $('#clicker').click(function() {
       $.ajax({
         dataType: "json",
-        url: "https://emojihub.yurace.pro/api/random.json",
+        url: "https://api.thecatapi.com/v1/images/search",
         success: function(results) {
           console.log(results["url"]);
-            $('#dog').attr("src", results["url"]);
+          $('#dog').attr("src", results["url"]);
+    
         },
         error: function(xhr,status,error) {
           console.log(error);
