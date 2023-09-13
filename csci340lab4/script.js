47f1f3a1fe5aae699dc6fbc7438b2ae93166e316
Ketsia <dusengeketty1@gmail.com>
2023-09-13 12:29:45 -0500

    added a second ajax call in the same button

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -4,13 +4,30 @@ $(document).ready(function() {
         dataType: "json",
         url: "https://api.thecatapi.com/v1/images/search",
         success: function(results) {
-          console.log(results["url"]);
-          $('#dog').attr("src", results["url"]);
+          console.log(results[0]["url"]);
+          $('#dog').attr("src", results[0]["url"]);
     
         },
         error: function(xhr,status,error) {
           console.log(error);
         }
       });
+      $.ajax({
+        dataType:"json",
+        url:"https://meowfacts.herokuapp.com/",
+        success: function(results){
+          console.log(results["data"][0]);
+          var apianswer = results["data"][0];
+          document.getElementById("catdescr").innerHTML = apianswer
+          $('#catdescr').attr("src", results["data"][0]);
+        },
+        error: function(xhr,status,error){
+          console.log(error)
+        }
+      }
+      )
+    });
+    $('#clicker2').click(function(){
+      
     });
   });
