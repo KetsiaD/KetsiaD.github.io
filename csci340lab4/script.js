d1fe2bb8abca04fb8800d8a6242331cce47d5964
Ketsia <dusengeketty1@gmail.com>
2023-09-13 11:48:28 -0500

    added image files and an api to generate random images

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -2,14 +2,10 @@ $(document).ready(function() {
     $('#clicker').click(function() {
       $.ajax({
         dataType: "json",
-        url: "https://random.dog/woof.json",
+        url: "https://emojihub.yurace.pro/api/random.json",
         success: function(results) {
           console.log(results["url"]);
-          if (results["url"].endsWith(".mp4")) {
-            $('#dog').attr("src", "images/blank.png");
-          } else {
             $('#dog').attr("src", results["url"]);
-          }
         },
         error: function(xhr,status,error) {
           console.log(error);
